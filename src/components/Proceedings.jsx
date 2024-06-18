45912d086f9f686d6e5f9e8bb441aa6eb6c4a792
aleksandrsokolov <timac11@bk.ru>
2024-06-18 18:56:57 +0300

    updates, add ccis link

--- src/components/Proceedings.jsx
+++ src/components/Proceedings.jsx
@@ -5,8 +5,8 @@ export default function Proceedings() {
     <>
       <div className="md:leading-7 md:text-xl">
         <p className="">
-          We are pleased to announce that the DCCN 2023 post-proceedings book (LNCS, volume 14123) has been published by Springer Nature.
-          The book is available online.
+          We are pleased to announce that both of the DCCN 2023 post-proceedings books (LNCS, volume 14123, and CCIS, volume 2129) have been published by Springer Nature.
+          The books are available online.
         </p>
         <div className="flex mt-6">
           <div className="mr-5" style={{maxWidth: "140px", minWidth: "120px"}}>
@@ -26,21 +26,21 @@ export default function Proceedings() {
           </div>
         </div>
 
-        {/*<div className="flex mt-6">*/}
-        {/*  <div className="mr-5" style={{maxWidth: "140px", minWidth: "120px"}}>*/}
-        {/*    <img src="/images/ccis_cover.png" alt="CCIS Volume cover"/>*/}
-        {/*  </div>*/}
-        {/*  <div style={{wordBreak: "break-word"}}>*/}
-        {/*    <p className="font-semibold">*/}
-        {/*      CCIS 1552:*/}
-        {/*      <br />*/}
-        {/*      <a className="text-blue-500" target="_blank" href="https://link.springer.com/book/10.1007/978-3-030-97110-6">https://link.springer.com/book/10.1007/978-3-030-97110-6</a>*/}
-        {/*    </p>*/}
-        {/*    <p className="mt-8">*/}
-        {/*      The access to the CCIS volume is granted for the period from March 7 to April 7, 2023.*/}
-        {/*    </p>*/}
-        {/*  </div>*/}
-        {/*</div>*/}
+        <div className="flex mt-6">
+          <div className="mr-5" style={{maxWidth: "140px", minWidth: "120px"}}>
+            <img src="/images/ccis_cover.png" alt="CCIS Volume cover"/>
+          </div>
+          <div style={{wordBreak: "break-word"}}>
+            <p className="font-semibold">
+              CCIS 2129:
+              <br />
+              <a className="text-blue-500" target="_blank" href="https://link.springer.com/book/10.1007/978-3-031-61835-2">https://link.springer.com/book/10.1007/978-3-031-61835-2</a>
+            </p>
+            {/*<p className="mt-8">*/}
+            {/*  The access to the CCIS volume is granted for the period from March 7 to April 7, 2023.*/}
+            {/*</p>*/}
+          </div>
+        </div>
       </div>
     </>
   )
